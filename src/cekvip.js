ac098d8552635f664c92229f26630c41d3342516
Hiosaki-files <79849089+Hiosaki-files@users.noreply.github.com>
2021-03-09 05:40:07 -0300

    Add files via upload

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  YUNO
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*MEMBRO VIP`
+}
+exports.cekvip = cekvip
