2f88dd986e5102752ece4a8340ddc47160c768a0
Hiosaki-files <79849089+Hiosaki-files@users.noreply.github.com>
2021-03-09 05:36:42 -0300

    Delete src/plugins directory

--- src/plugins/creator.js
+++
@@ -1,9 +0,0 @@
-let handler = function (m) {
-  this.sendContact(m.chat, '5538988313755', 'Nurutomo', m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler
